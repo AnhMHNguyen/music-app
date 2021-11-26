595a98381e6443097fe32ae5c5c5bbfc30ff8658
AnhMHNguyen <84159499+AnhMHNguyen@users.noreply.github.com>
2021-11-26 00:06:33 -0800

    initial commit

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -4,11 +4,11 @@ import 'firebase/firestore';
 import 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyC8U58ob35uJj74ZdPu5DFWH4FSsozIzCs',
-  authDomain: 'music-55377.firebaseapp.com',
-  projectId: 'music-55377',
-  storageBucket: 'music-55377.appspot.com',
-  appId: '1:357165514899:web:05000791f2631c35a10d66',
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE,
+  appId: process.env.VUE_APP_API_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
